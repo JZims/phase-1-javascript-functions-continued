63b96c032558713c4694018b37c8f5039c3e3fee
JZims <JZimmsGames@gmail.com>
2021-03-17 01:02:48 -0400

    commit tree e8268d27ef717c40d496be03f38aea15481e2bbd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    let intro = "This Saturday, I want to"
+    return `${intro} ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    let intro = "This Monday, I will"
+    return `${intro} ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+           return function(adjective="special") {
+            
+            return `You are ${flair}${adjective}${flair}!`
+    
+        }
+    
+}
